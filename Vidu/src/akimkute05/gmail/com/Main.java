cf7b099be4d207202450b84699d9aab33427679f
HOCVIEN <HOCVIEN@DESKTOP-LMHOMK8>
2018-03-31 16:36:58 +0700

    New

--- Vidu/src/akimkute05/gmail/com/Main.java
+++ Vidu/src/akimkute05/gmail/com/Main.java
@@ -1,11 +1,21 @@
 package akimkute05.gmail.com;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-	public static void main(String[] args) {
-		System.out.println("Hello Java");
-		System.out.println("Hello World");
-		
-		
+	public static void main(String[] args) throws IOException {
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
+		String a = buf.readLine();
+		String b = buf.readLine();
+		float c = Float.parseFloat(a);
+		float d = Float.parseFloat(b);
+		System.out.println("tao la ket qua " + "\n"+ (c + d));
+
 	}
 }
