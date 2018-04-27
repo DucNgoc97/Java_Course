d69c322535aea2107fcd8cbd5da34e2e344bde74
DucNgoc97 <37949697+DucNgoc97@users.noreply.github.com>
2018-04-27 09:43:32 +0700

    Update KhachHang.java

--- Java_Course02/src/akimkute10/gmail/com/KhachHang.java
+++ Java_Course02/src/akimkute10/gmail/com/KhachHang.java
@@ -1,28 +1,47 @@
 package akimkute10.gmail.com;
 
+import java.util.Scanner;
+
 public class KhachHang {
 	private String username;
-	private String password;
+	//Tiếp tục cho các thuộc tính khác
 
-	public void setUsername(String name) {
-		username = name;
-	}
+	private String password;
 
 	public String getUsername() {
 		return username;
 	}
 
-	public void setPassword(String pass) {
-		password = pass;
+	public void setUsername(String usn) {
+		username = usn;
 	}
+	//Tiếp tục cho các phương thức
 
 	public String getPassword() {
 		return password;
 	}
 
-	public KhachHang() {
-		username = "abc";
-		password = "123";
+	public void setPassword(String pw) {
+		password = pw;
 	}
 
+	public KhachHang(String name, String pw) {
+		username = name;
+		password = pw;
+	}
+
+	public KhachHang() {
+		username = "admin"; // khởi tạo username = admin
+		password = "123"; // khởi tạo password = 123
+	}
+	// gán các giá trị cho phiếu mua hàng
+	public String MuaHang(String th, String nsx, double dg, int sl) {
+			HangHoa hh = new HangHoa();
+			hh.setTenHang(th);
+			hh.setNgaySanXuat(nsx);
+			hh.setDonGia(dg);
+			hh.setSoLuong(sl);
+			hh.TinhTien();
+			return hh.toString();
+		}
 }
