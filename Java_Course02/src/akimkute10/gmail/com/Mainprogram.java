57196f62e568ddd7eecc78b00eecf61af75b41fb
DucNgoc97 <37949697+DucNgoc97@users.noreply.github.com>
2018-04-27 09:42:46 +0700

    Update Mainprogram.java

--- Java_Course02/src/akimkute10/gmail/com/Mainprogram.java
+++ Java_Course02/src/akimkute10/gmail/com/Mainprogram.java
@@ -1,25 +1,41 @@
-package akimkute10.gmail.com;
+papackage akimkute05.gmail.com;
 
 import java.util.Scanner;
 
-public class Mainprogram {
+public class MainProgram {
 
 	public static void main(String[] args) {
-		HangHoa hh = new HangHoa("teo", "20", 10, 03);
+		String mua = new String();
 		KhachHang kh = new KhachHang();
-		String a, b;
-		hh.setTenHang("Bánh");
-		hh.setNgaySanXuat("1/1/2018");
-		hh.setSoLuong(4);
-		hh.setDonGia(50000);
-		System.out.println(hh.inThongTin());
+		// Tiếp tục với các yêu cầu của đề bài
 		Scanner sc = new Scanner(System.in);
-		a = sc.nextLine();
-		b = sc.nextLine();
-		if ((a.equals(kh.getusername())) && (b.equals(kh.getpassword())))
-			System.out.println("Số tiền bạn phải thanh toán là " + hh.tinhTien());
-		else
-			System.out.println("Xin lỗi tài khoản của quý khách không tồn tại trong hệ thống");
+		int n;
+		System.out.println("Mời bạn nhập username");
+		String usn = sc.nextLine();
+		System.out.println("Mời bạn nhập password");
+		String pass = sc.nextLine();
+		if (usn.equals(kh.getUsername()) && pass.equals(kh.getPassword())) {
+			do {
+				System.out.println("Bạn có muốn mua hàng");
+				System.out.println("1. Có");
+				System.out.println("2. Không");
+				n = sc.nextInt();
+				if (n == 1) {
+					Scanner sc1 = new Scanner(System.in);
+					System.out.println("Nhập tên hàng");
+					String ten = sc1.nextLine();
+					System.out.println("Nhập ngày sản xuất");
+					String ngaysx = sc1.nextLine();
+					System.out.println("Nhập số lượng");
+					int sl = sc1.nextInt();
+					System.out.println("Nhập Đơn giá");
+					double dg = sc1.nextDouble();
+					mua += "\n------\n" + kh.MuaHang(ten, ngaysx, dg, sl);
+				} else
+					System.out.println(mua);
+			} while (n == 1);
+		} else
+			System.out.println("tài khoản của quý khách không tồn tại trong hệ thống."
+					+ " Xin liên hệ chi nhánh khách hàng gần nhất");
 	}
-
 }
