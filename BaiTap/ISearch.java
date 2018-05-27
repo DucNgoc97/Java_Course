600d865f4c98044d8bccc048a389b59ca3b0f55b
DucNgoc97 <37949697+DucNgoc97@users.noreply.github.com>
2018-05-28 06:20:14 +0700

    Add files via upload

---
+++ BaiTap/ISearch.java
@@ -0,0 +1,10 @@
+package akimkute05.gmail.com;
+
+public interface ISearch {
+
+	public Object TimKiem(int from, int to);
+
+	public Object TimKiem(boolean gt);
+
+	public Object TimKiem(String ten);
+}
